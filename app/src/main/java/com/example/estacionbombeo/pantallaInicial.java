0826e0978a30d199f57677e3f2f5d70af795f9ae
angelswat <angaenca@espol.edu.ec>
2020-03-16 19:01:39 -0500

    Pantalla inicial de prueba

---
+++ app/src/main/java/com/example/estacionbombeo/pantallaInicial.java
@@ -0,0 +1,14 @@
+package com.example.estacionbombeo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pantallaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicial);
+    }
+}
