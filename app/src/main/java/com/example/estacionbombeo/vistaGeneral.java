fe43849188fe08b501537a77115ceddfee3a8afb
Lenin Pardo Salazar <lapardo@espol.edu.ec>
2020-03-19 11:04:38 -0500

    import mysql

--- app/src/main/java/com/example/estacionbombeo/vistaGeneral.java
+++ app/src/main/java/com/example/estacionbombeo/vistaGeneral.java
@@ -1,7 +1,11 @@
 package com.example.estacionbombeo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import android.content.Intent;
 import android.os.Bundle;
 
