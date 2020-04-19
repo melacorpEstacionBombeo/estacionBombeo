0ee7438b46652eaa25cd73ebd2168da0c85dbb2a
alfachino216 <lapardo505@gmail.com>
2020-04-18 22:07:52 -0500

    correcciones a vista general

---
+++ app/src/main/java/com/example/estacionbombeo/Historicos.java
@@ -0,0 +1,15 @@
+package com.example.estacionbombeo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Historicos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historicos);
+
+    }
+}
